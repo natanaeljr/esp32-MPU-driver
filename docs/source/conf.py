44f252489e96143fd35c9f93ea0039ce235357a5
Natanael Rabello <natanael.rabello@outlook.com>
2018-02-23 13:24:02 -0300

    Add requirements.txt to build docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -164,3 +164,15 @@ texinfo_documents = [
      author, 'MPUDriver', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+
+# -- Use sphinx_rtd_theme for local builds --------------------------------
+# ref. https://github.com/snide/sphinx_rtd_theme#using-this-theme-locally-then-building-on-read-the-docs
+#
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
